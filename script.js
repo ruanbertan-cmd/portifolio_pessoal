a755c365d2bf4a420713baf01f0b53da551105c9
Ruan Bertan <ruan.bertan@gmail.com>
2025-09-19 10:55:36 -0300

    fix: ajuste nas mesagens decodificadas

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('index.html');addEventListener('submit', function(event)
     const nome = document.getElementById('nome').value;
     const comentario = document.getElementById('comentario').value;
     
-    const mensagemParaWhatsApp = `Nome: ${encodeURIComponent(nome)}\nComentario: ${encodeURIComponent(comentario)}`;
+    const mensagemParaWhatsApp = `Nome: ${(nome)}\nComentario: ${(comentario)}`;
     const numeroWhatsApp = '5548998419856';
     
     const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagemParaWhatsApp)}`;
